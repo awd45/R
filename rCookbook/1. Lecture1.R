4c4c227f243abca297806d7a8d9e732b1eb9dfb2
kimhyokwan <haiteam@naver.com>
2018-07-25 14:56:52 +0900

    .

---
+++ rCookbook/1. Lecture1.R
@@ -0,0 +1,443 @@
+
+a<-1
+
+# Interactive environment
+pi
+sqrt(2)
+# Creating vector
+c(1,2,3,4)
+
+# Defining variables
+a=1
+a<-1
+############################
+####### 2.1 Printing Something
+############################
+#Creating vector
+tempVector <- c(1,2,3,4)
+#average
+mean(x)
+#standard deviation
+sd(x)
+#variance
+var(x)
+
+# show function details
+?matrix
+
+# Crete matrix byrow <- fill the value using column order
+x <- matrix( c(1,2,3,4), 2,2)
+y <- matrix(tempVector,2,2)
+# Crete matrix byrow <- fill the value using row order
+z <- matrix(tempVector,2,2, byrow=2)
+
+
+# print
+cat("This is ML Class")
+cat("This is ML Class\n")
+
+sqrt(x^2)
+
+a=3.5
+sprintf("pi is %f \n",a)
+
+############################
+####### 2.2 Setting Variables
+############################
+
+x<-3
+x=3
+
+x <-3
+y <-4
+z <- sqrt(x^2 + y^2)
+print(z)
+
+# Creating vector
+# Select specific vector point
+x <- c(1,2,3,4)
+x[1]
+
+y <- c("fee","fie","foe","fun")
+y[1]
+
+z <- 3
+
+############################
+####### 2.3 Listing Variables
+############################
+
+### Generate Function
+f <- function(n,p) sqrt(p*(1-p)/n)
+f
+f(2,1/2)
+
+### Generate Function
+## Define of Function
+g <- function (a,b) {
+  u = a+b
+  v = a-b
+  # Do not use return
+  v / u}
+
+## Use of Function
+g(3,2)
+
+############################
+####### 2.4 Deleting Variables
+############################
+# showing list of all variables
+ls()
+?rm
+# deleting specific variable
+rm(x)
+# deleting all varaiables
+rm(list=ls())
+
+############################
+####### 2.5 Creating a Vector
+############################
+## Variables Uppercase LowerCase
+c(1,1,2,3,5,8,13,21)
+
+k = c( TRUE, TRUE FALSE, TRUE)
+K
+
+## Logical operator
+l = c(1==2,1==1)
+l
+
+v1 = c(1,2,3,4)
+v2 = c(4,5,6)
+v1
+v2
+
+## concating vector
+w = c(v1,v2)
+w
+
+v1
+## concating vector and type is cenverted to String 
+## if new vector value can not be cast to original type
+v3 <- c("A","B","C")
+w1 <- c(v1,v3)
+w1
+
+## Finding type of variable (class)
+#mode(w1)
+class(w1)
+
+############################
+####### 2.6 Basic Statistics
+############################
+x <- c(0,1,1,2,3,5,8,13,21,34)
+
+mean(x)
+median(x)
+sd(x)
+var(x)
+
+# r feature
+# (Do not use loop -> vector operation)
+y <- log(x+1)
+z <- x+1
+z
+
+# Calculating corr
+cor(x,y)
+
+rm(list=ls())
+
+############################
+####### 2.7 Sequence !!!!!!!!
+############################
+x = c(1:100)
+y = c(100:1)
+x
+
+f = c(1:100)
+y = 2 * f +1
+
+lm_r = lm ( y ~ f)
+
+coef(lm_r)
+
+summary(lm_r)
+resid(lm_r) 
+fitted(lm_r) 
+plot(lm_r)
+
+# Well
+data()
+
+
+############################
+####### Etc
+############################
+## Setting working directory
+getwd()
+setwd("D://rLecture//rCookbook")
+
+x = c(1:100)
+y = c(100:1)
+x
+
+f = c(1:100)
+y = 2 * f +1
+
+lm_r = lm ( y ~ f)
+
+coef(lm_r)
+
+summary(lm_r)
+resid(lm_r) 
+fitted(lm_r) 
+plot(lm_r)
+
+lm_r
+
+# Wellknown data
+data()
+
+## Loading data
+data(iris)
+## Checking iris data
+head(iris)
+
+## book introduction type:pdf
+islr type:pdf
+
+## packge Install
+install.packages("ISLR")
+## library load
+library(ISLR)
+## Searching library
+search()
+
+############################
+####### 3.5. Displaying the Search Path
+############################
+# Searching Currnetly loaded library
+search()
+
+# MASS
+# MASS Wellknow package
+# Modern Applied Statistics With S 
+library("MASS")
+search()
+
+############################
+####### 3.12 Running a Script
+############################
+source("Runnable.R")
+
+############################
+####### 3.13 Running a Batch Script
+############################
+# Setting System Variables
+# R CMD BATCH scriptfile outputfile
+# R CMD BATCH Runnable.R log.txt
+# C:\Program Files\R\R-3.5.1\bin\x64
+
+
+#####################################
+# 4.2 Printing Fewer Digits (or More Digits)
+#####################################
+q <- seq(from =0 , to =3, by = 0.5)
+# normalization  avg=0 stdev = 1
+pnorm(q)
+rount(pnorm(-q),3)
+
+#####################################
+# Create Dataframe
+testDf <- data.frame(Quant = q, Lower = pnorm(-q), Upper = pnorm(q))
+testDf
+
+
+dateIndex = c('2017-12-01','2017-12-02','2017-12-03','2017-12-04','2017-12-05','2017-12-06')
+productNameList = c('REF','TV','MOBILE','REF','MOBILE',"")
+volumeList = c(1000,2000,1500,1200,1700,1000)
+
+testDf <- data.frame( date = dateIndex,
+                    product = productNameList,
+                    volume = volumeList)
+### Dataframe select
+### Selecting column
+#install.packages("data.table")
+library(data.table) 
+
+## dataframe
+## datatable
+testDt <- data.table(testDf) 
+
+## selecting Specific Columns
+testDtSelectColumns = testDt[, .(product)]
+  
+## selecting Specific Row
+testDtSelectRows = testDt[product %in% c("REF","TV") & volume >= 1000]
+testDtSelectRows = testDt[product == "REF" & volume >= 1000]
+
+#####################################
+# 4.2 Printing Fewer Digits (or More Digits)
+#####################################
+
+#####################################
+# 4.5 Dealing with ¡°Cannot Open File¡± in Windows
+#####################################
+getwd()
+setwd("D://rLecture//rCookbook")
+samp <- read.csv("./data/data.csv")
+samp
+?read.csv
+# header = FASLE , sep option can be changed
+samp <- read.csv("./data/data.csv", header=FALSE)
+samp
+
+#####################################
+# 4.10 Reading Tabular or CSV Data from the Web
+#####################################
+
+#####################################
+# 4.13 Reading from MySQL Databases
+#####################################
+
+#####################################
+# Chapter 5 Data Structure
+#####################################
+
+#Vector, Lists
+
+#####################################
+# 5.2 Inserting Data into a Vector
+#####################################
+1:10
+
+append(1:10, 99, after = 5)
+append(1:10, 99, after = 0)
+
+# cbind rbind
+x0 <- 1:10
+y0 <- 10:19
+z0 <- cbind(x0,y,y0)
+
+x <- 1:10
+y <- 10:19
+z1 <- cbind(x,y)
+z
+a <- 1:6
+# if row count is not matching iter the value
+z2 <- cbind(x,a)
+# rbind must match number of columns
+z3 <- rbind(z1,z0)
+
+# Vector
+
+# Summarizing Data
+# Feature, Factor
+head(iris)
+summary(iris)
+
+# Select specific column
+iris$Sepal.Length
+
+# Select specific row using index
+iris$Sepal.Length[10]
+# Select specific row using value
+iris[iris$Sepal.Length==4.9,]
+
+# vector operation
+a <- c(1:5)
+b <- c(2:6)
+a*b
+
+#####################################
+# 5.18 Initializing a Data Frame from Column Data
+####################################
+# difference between matrix and dataframe
+# matrix has only numeric value
+
+getwd()
+setwd("D://rLecture//rCookbook")
+
+?read.csv
+# header = FASLE , sep option can be changed
+samp <- read.csv("./data/data.csv", header=FALSE)
+samp
+class(samp)
+samp
+
+## Extracting data using condition
+## using condition <- subset is more frequent
+s <- subset(samp, subset = (V2 > 2))
+t <- subset(samp, subset = (V2 > 2))$V2
+t
+
+ans <- samp[samp$V2>2,]$V2
+ans
+
+#####################################
+#5.25 Changing the Names of Data Frame Columns
+#####################################
+# Changing matrix data to dataframe
+x <- matrix( c(1,2,3,4), 2,2)
+y <- matrix(tempVector,2,2)
+z <- matrix(tempVector,2,2, byrow=2)
+z
+class(z)
+df2 <-as.data.frame(z)
+class(df2)
+df2
+
+# Changing column name in Dataframe
+colnames(df2) <- c("before","treatment")
+df2
+
+#####################################
+#apply function -> column create using
+#####################################
+
+#######################################
+# Drawing chart
+#######################################
+
+# delete all variables
+rm(list=ls())
+
+#rnorm (random normalization mean = 0 , sd = 1)
+?rnorm
+x = rnorm(100)
+
+# avg = 10 std = 1
+# create dataset 
+# y = 2 * x + 1
+x = rnorm(100,10,1)
+y = 2 * x + 1
+plot(x,y)  
+# y = 2 * x + 1   + error
+# x = c(1:100)
+# e ÀÔ½Ç·Ð = rnorm(100,0,1)
+e = rnorm(100,0,1)
+e = rnorm(100, sd = 5)
+e  
+y = 2 * x + 1 + e
+plot(x,y)
+
+# 
+x = c(1:100)
+y = 2* x +1 +e
+plot(x,y)
+
+
+x = rnorm(100, mean = 10, sd = 5)
+e = rnorm(100, sd = 5)
+y = 3 * x + 10 + e
+
+plot(x,y)
+# lm model
+?lm
+l = lm(y ~ x)
+abline(l)
+l
+
+# pvalue -> if pvalue is less than 0.05 it is meaningful model
+summary(l)
---
+++ rCookbook/3. Lecture1-ISLR.R
@@ -0,0 +1,236 @@
+############################################################
+############## Remove all packages #########################
+# create a list of all installed packages
+ip <- as.data.frame(installed.packages())
+head(ip)
+# if you use MRO, make sure that no packages in this library will be removed
+ip <- subset(ip, !grepl("MRO", ip$LibPath))
+# we don't want to remove base or recommended packages either\
+ip <- ip[!(ip[,"Priority"] %in% c("base", "recommended")),]
+# determine the library where the packages are installed
+path.lib <- unique(ip$LibPath)
+# create a vector with all the names of the packages you want to remove
+pkgs.to.remove <- ip[,1]
+head(pkgs.to.remove)
+# remove the packages
+sapply(pkgs.to.remove, remove.packages, lib = path.lib)
+############################################################
+############################################################
+
+### i
+
+getwd()
+setwd("D:/rLecture/rCookbook")
+
+install.packages("ISLR")
+## library load
+search()
+library(MASS)
+library(ISLR)
+search()
+
+
+#################################
+# 3. Linear Regression (p 127)
+#################################
+# if fix is not working go to Environment Global Environment
+# Clear object
+# https://stackoverflow.com/questions/24587980/fix-function-not-working-in-r
+
+fix(college)
+names(Boston)
+
+lm.fit =lm(medv ~ lstat)
+lm.fit =lm(medv ~ lstat , data=Boston )
+attach (Boston )
+lm.fit =lm(medv ~ lstat)
+lm.fit
+
+#################################
+# 3.6.3 Multiple Linear Regression (p 128)
+##################################
+lm.fit =lm(medv ~ lstat + age , data=Boston )
+lm.fit
+
+# . = all data
+lm_fit = lm( medv ~. , data=Boston)
+lm_fit
+# if pvalue is less than 0.05 it is not related to medv
+summary(lm_fit)
+
+############################
+# Feature selection
+############################
+dim(Boston)
+# 506 count 14 features
+
+# if data has 13 features
+# (forward method)
+# set feature then add feature one by one from the first feature
+# (backward method) 
+# set all feature (13) then delete feature one by one
+
+###############################
+# Epsilon
+# y = ax + ax^2 + ax^3 + E (E)
+
+lm_fit2 =  lm(medv ~ lstat + I(lstat^2) + I(lstat^3) + I(lstat^4)     )
+lm_fit2
+summary(lm_fit2)
+
+####
+# 3.6.6 Qualitative Predictors
+############################
+
+# carsheet display
+#We will attempt to predict Sales (child car seat sales) 
+# in 400 locations
+# based on a number of predictors.
+
+# bad medium, goot
+
+fix(Carseats)
+names(Carseats)
+
+lm_fit = lm(Sales ~. + Income :Advertising +Price :Age , data=Carseats)
+
+lm_fit
+
+summary(lm_fit)
+
+attach (Carseats )
+contrasts (ShelveLoc )
+
+LoadLibraries=function (){
+   library (ISLR)
+   library (MASS)
+   print (" The libraries have been loaded .")
+}
+#library
+search()
+#packages
+installed.packages()
+
+LoadLibraries
+
+
+################################
+# 4.6 Lab: Logistic Regression, LDA, QDA, and KNN
+######################################
+
+# f(x) = e^x / 1+ e^x
+
+
+# compare up and down of stock price
+
+library (ISLR)
+names(Smarket)
+
+Smarket
+# 2001~ 20015
+
+summary(Smarket)
+# 2001 ~ 2005
+# Today up/down compare to yesterday
+
+# minus -9 index column
+# Year""Lag1""Lag2" "Lag3""Lag4""Lag5""Volume" "Today""Direction"   
+
+
+
+# correlation 
+# +1 plus cor 
+# -1 minus cor
+head(Smarket)
+names(Smarket)
+cor(Smarket[,-9])
+
+head(Smarket[-9])
+
+attach (Smarket )
+plot(Volume )
+
+#############################
+# 4.6.2 Logistic Regression
+#############################
+### Classification 0 or 1
+### P = ex/1+ex
+#This data set consists of
+#percentage returns for the S&P 500 stock index over 1, 250 days, from the
+#beginning of 2001 until the end of 2005. For each date, we have recorded
+#the percentage returns for each of the five previous trading days, Lag1
+#through Lag5. We have also recorded Volume (the number of shares traded
+#                                            4.6 Lab: Logistic Regression, LDA, QDA, and KNN 155
+#                                            on the previous day, in billions
+glm_fit=glm(Direction ~ Lag1+Lag2+Lag3+Lag4+Lag5+Volume ,
+            data=Smarket ,family =binomial )
+
+summary (glm_fit )
+
+coef(glm_fit)
+
+summary (glm_fit )$coef
+
+summary (glm_fit )$coef [,4]
+
+
+glm.probs =predict (glm_fit ,type ="response")
+glm.probs [1:10]
+
+# defining up -> 1 down -> 0
+contrasts (Direction )
+
+
+glm.pred = rep("Down", 1250)
+
+# if the prob value is upper than 0.5 put "Up"
+glm.pred[ glm.probs > 0.5] ="Up"
+
+table(glm.pred, Direction)
+
+Direction
+
+?Predict
+
+# train True/False
+train =(Year <2005)
+
+# get Data if train is false
+
+Smarkek_2000= Smarket [ train ,]
+Smarkek_2005= Smarket [! train ,]
+
+Direction_2005 = Smarkek_2005["Direction"]
+
+summary(Smarkek_2005)
+
+dim(Smarkek_2000)
+dim(Smarkek_2005)
+dim(Direction_2005)
+
+Smarkek_2000
+
+#### Training 
+# binomial -> logistic
+# subset = train -> use only training set
+glm_fit=glm(Direction ~ Lag1+Lag2+Lag3+Lag4+Lag5+Volume ,
+            data=Smarket ,family =binomial , subset = train )
+
+glm_probs = predict(glm_fit, Smarkek_2005, type="response")
+# Walk forward
+
+glm_probs
+dim(Smarkek_2005)
+
+glm_pred = rep("Down", 252)
+glm_pred[ glm_probs > 0.5] = "Up"
+
+glm_pred
+
+final_result = cbind(Smarkek_2005,glm_pred)
+
+final_result$Compare = final_result$Direction == final_result$glm_pred
+
+final_result
+
+ls()
